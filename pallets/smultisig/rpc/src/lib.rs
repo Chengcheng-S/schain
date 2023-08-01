171c4229f81fa9f89b84c73caaeb606f1558647e
Cheng <87012234+Chengcheng-S@users.noreply.github.com>
2023-08-01 11:28:25 +0800

    rpc methods

--- pallets/smultisig/rpc/src/lib.rs
+++ pallets/smultisig/rpc/src/lib.rs
@@ -16,3 +16,40 @@
 // limitations under the License.
 
 //! RPC interface for the transaction payment pallet.
+
+use std::{convert::TryInto, sync::Arc};
+
+use codec::{Codec, Decode};
+use jsonrpsee::{
+	core::{Error as JsonRpseeError, RpcResult},
+	proc_macros::rpc,
+	types::error::{CallError, ErrorCode, ErrorObject},
+};
+
+
+use smultisig_rpc_runtime_api::SmultisigApi;
+
+
+#[rpc(client,server)]
+pub trait SumtisigModuleApi {
+
+    #[method(name = "group_members")]
+    fn group_members(&self)->Vec<AccountId>;
+
+
+
+}
+
+/// Provides RPC methods to query a dispatchable's class, weight and fee.
+pub struct SmultisigModule<C, P> {
+	/// Shared reference to the client.
+	client: Arc<C>,
+	_marker: std::marker::PhantomData<P>,
+}
+
+impl<C, P> SmultisigModule<C, P> {
+	/// Creates a new instance of the TransactionPayment Rpc helper.
+	pub fn new(client: Arc<C>) -> Self {
+		Self { client, _marker: Default::default() }
+	}
+}
