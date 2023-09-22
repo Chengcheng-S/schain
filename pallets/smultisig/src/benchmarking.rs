3f9ae36fdfcbf3dd2932b111392ab87ef636e316
Chengcheng-S <87012234+Chengcheng-S@users.noreply.github.com>
2023-09-22 12:26:40 +0800

    fix some code

--- pallets/smultisig/src/benchmarking.rs
+++ pallets/smultisig/src/benchmarking.rs
@@ -6,23 +6,24 @@ use super::*;
 #[allow(unused)]
 use crate::{Pallet as Smultisig};
 use frame_benchmarking::v1::{benchmarks,account,impl_benchmark_test_suite};
-use frame_system::RawOrigin;
+use frame_support::dispatch::RawOrigin::Root;
 use sp_std::vec;
 fn create_user<T: Config>(string: &'static str, n: u32, seed: u32) -> T::AccountId {
     let user = account(string, n, seed);
-
     user
 }
 
 benchmarks!{
     create_multisig_group{
-        let caller = create_user("caller",0u32,1u32);
-        let member_one = create_user("member_one",0u32,1u32);
-        let member_two = create_user("member_two",0u32,1u32);
-        let members = vec![caller,member_one,member_two];
-    }:_(RawOrigin::Signed(&caller),members)
+        let caller = create_user::<T>("caller",0u32,1u32);
+        let member_one = create_user::<T>("member_one",0u32,1u32);
+        let member_two = create_user::<T>("member_two",0u32,1u32);
+        let root = Root.as_signed().unwrap();
+        let members = vec![root,&caller,&member_one,&member_two];
+    }:_(Root,members)
     verify{
-        assert_eq!(Smultisig::members().contains(&member_two),true);
+        assert_eq!(Smultisig::<T>::members().contains(&member_two),true);
+        assert_eq!(Smultisig::<T>::members().contains(&member_one),true);
     }
 }
 impl_benchmark_test_suite!(Smultisig, crate::mock::new_test_ext(), crate::mock::Test);
